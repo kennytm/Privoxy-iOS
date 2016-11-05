a1c38d701ff2ae150fe2aff4a586d876f660c112
kennytm <kennytm@gmail.com>
2016-11-05 19:30:56 +0800

    Added import script.

---
+++ import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -euo pipefail
+
+VERSION=3.0.26
+
+curl -O "https://www.privoxy.org/sf-download-mirror/Sources/${VERSION}%20%28stable%29/privoxy-${VERSION}-stable-src.tar.gz"
+tar xf "privoxy-${VERSION}-stable-src.tar.gz"
+rm "privoxy-${VERSION}-stable-src.tar.gz"
+mv "privoxy-${VERSION}-stable" src
